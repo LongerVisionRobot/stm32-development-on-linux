7e4ef25f8688aad5eae811b0aabd24ea8875cd71
Lixingcong <lixingcong@users.noreply.github.com>
2016-04-22 01:27:54 +0800

    [cp_c-files_to_tmp.sh]: iconv for converting from GBK to UTF

--- cp_c-files_to_tmp.sh
+++ cp_c-files_to_tmp.sh
@@ -5,12 +5,24 @@ INDENT="        "
 
 if [ "$#" != "1"  ]; then
 	echo "${INDENT}copy all the c source file in the current dir to /tmp/ccc"
-	echo "${INDENT}except stm32f10xxxxx.*"
 	echo "${INDENT}USAGE:"
 	echo "${INDENT}${INDENT}$0 ."
 	exit 1
 fi
 
+echo 'Would you like to use iconv to convert from GBK to UTF8?'
+echo -n 'if no, just copy your files. (y/n):'
+while [ True ];do
+	read is_conv
+	if [ ${is_conv} != "y" ] && [ ${is_conv} != "n" ];then
+		echo "please answer y or n."
+		continue
+	else
+		break
+	fi
+done	
+
+# Make dir
 if [ ! -d "/tmp/ccc" ];then
 	mkdir /tmp/ccc
 else
@@ -19,23 +31,27 @@ fi
 
 dir="$@"
 find $@ |
-tail -n +2 |
-while read line
-do
-	filename=${line##*/}
-	#if [ ${filename%f10x*.*} = "stm32" ];then
-	#	continue
-	#fi
-
-	if [ -f ${line} ];then
-		echo "bash: cp ${line} /tmp/ccc/"
-		cp ${line} /tmp/ccc/
-		if [ $? != "0" ];then
-			echo ERROR
-			exit 1
+	tail -n +2 |
+	while read line
+	do
+		filename=${line##*/}
+		# Ignore folders
+		if [ -f ${line} ];then
+			if [ ${is_conv} = "n" ];then
+				echo "bash: cp ${line} /tmp/ccc/"
+				cp ${line} /tmp/ccc/
+			else
+				echo "bash: iconv -f gbk -t utf-8 ${line} > /tmp/ccc/${filename}"
+				iconv -f gbk -t utf-8 ${line} > /tmp/ccc/${filename}
+			fi
+			# check if executed successfully
+			if [ $? != "0" ];then
+				echo ERROR
+				exit 1
+			fi
+			# Add OBJ to OBJS
+			if [ ${filename##*.} = "c" ];then
+				echo "OBJS+=${filename%%.c}.o" >> /tmp/ccc/000000000_MAKE_OBJS.txt
+			fi
 		fi
-		if [ ${filename##*.} = "c" ];then
-			echo "OBJS+=${filename%%.c}.o" >> /tmp/ccc/000000000_MAKE_OBJS.txt
-		fi
-	fi
-done
+	done
